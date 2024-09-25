0167bd80db3e5be46b4b8c20d1a25bb7f7274bb1
Jumcee <futhmah456@gmail.com>
2024-09-25 22:31:10 +0100

    arbitrum-stylus

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,97 @@
-//!
-//! Stylus Hello World
-//!
-//! The following contract implements the Counter example from Foundry.
-//!
-//! ```
-//! contract Counter {
-//!     uint256 public number;
-//!     function setNumber(uint256 newNumber) public {
-//!         number = newNumber;
-//!     }
-//!     function increment() public {
-//!         number++;
-//!     }
-//! }
-//! ```
-//!
-//! The program is ABI-equivalent with Solidity, which means you can call it from both Solidity and Rust.
-//! To do this, run `cargo stylus export-abi`.
-//!
-//! Note: this code is a template-only and has not been audited.
-//!
-
-// Allow `cargo stylus export-abi` to generate a main function.
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
-/// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::U256, prelude::*};
+use stylus_sdk::{
+    alloy_primitives::{U256, Address},
+    msg,
+};
+use alloc::{collections::BTreeMap, vec::Vec, string::String};
+use alloc::format;
 
-// Define some persistent storage using the Solidity ABI.
-// `Counter` will be the entrypoint.
-sol_storage! {
-    #[entrypoint]
-    pub struct Counter {
-        uint256 number;
-    }
+// Define the TaskMarketplace struct manually
+pub struct TaskMarketplace {
+    tasks: BTreeMap<U256, (Address, Address, Vec<u8>, U256, bool)>,
+    task_count: U256,
 }
 
-/// Declare that `Counter` is a contract with the following external methods.
-#[public]
-impl Counter {
-    /// Gets the number from storage.
-    pub fn number(&self) -> U256 {
-        self.number.get()
+impl TaskMarketplace {
+    pub fn new() -> Self {
+        Self {
+            tasks: BTreeMap::new(),
+            task_count: U256::from(0),
+        }
     }
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
-        self.number.set(new_number);
+    pub fn create_task(&mut self, description: Vec<u8>, reward: U256) -> Result<U256, Vec<u8>> {
+        let task_id = self.task_count + U256::from(1);
+        self.tasks.insert(task_id, (msg::sender(), Address::ZERO, description, reward, false));
+        self.task_count = task_id;
+        Ok(task_id)
     }
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn mul_number(&mut self, new_number: U256) {
-        self.number.set(new_number * self.number.get());
+    pub fn accept_task(&mut self, task_id: U256) -> Result<(), Vec<u8>> {
+        let (creator, assignee, description, reward, completed) = self.tasks.get(&task_id).ok_or("Task not found")?;
+        if !assignee.is_zero() {
+            return Err("Task already assigned".into());
+        }
+        self.tasks.insert(task_id, (*creator, msg::sender(), description.clone(), *reward, *completed));
+        Ok(())
     }
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn add_number(&mut self, new_number: U256) {
-        self.number.set(new_number + self.number.get());
+    pub fn complete_task(&mut self, task_id: U256) -> Result<(), Vec<u8>> {
+        let (creator, assignee, description, reward, completed) = self.tasks.get(&task_id).ok_or("Task not found")?;
+        if *assignee != msg::sender() {
+            return Err("Not assigned to this task".into());
+        }
+        if *completed {
+            return Err("Task already completed".into());
+        }
+        self.tasks.insert(task_id, (*creator, *assignee, description.clone(), *reward, true));
+
+        // Commented out msg::send_value for now, as it's not available
+        // Transfer reward to assignee
+        // msg::send_value(assignee, reward);
+
+        Ok(())
     }
 
-    /// Increments `number` and updates its value in storage.
-    pub fn increment(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + U256::from(1));
+    pub fn get_task(&self, task_id: U256) -> Result<String, Vec<u8>> {
+        let (creator, assignee, description, reward, completed) = self.tasks.get(&task_id).ok_or("Task not found")?;
+        Ok(format!(
+            "{{\"creator\":\"{}\",\"assignee\":\"{}\",\"description\":\"{}\",\"reward\":\"{}\",\"completed\":{}}}",
+            creator,
+            assignee,
+            String::from_utf8_lossy(description).replace("\"", "\\\""),  // Updated for Vec<u8>
+            reward,
+            completed
+        ))
     }
-}
+
+    pub fn get_all_tasks(&self) -> String {
+        let mut all_tasks = String::from("[");
+        let task_count = self.task_count;
+        let mut first = true;
+    
+        // Fix for task_count iteration
+        for i in 1..=task_count.as_limbs()[0] {
+            if let Some((creator, assignee, description, reward, completed)) = self.tasks.get(&U256::from(i)) {
+                if !first {
+                    all_tasks.push_str(",");
+                } else {
+                    first = false;
+                }
+                all_tasks.push_str(&format!(
+                    "{{\"id\":\"{}\",\"creator\":\"{}\",\"assignee\":\"{}\",\"description\":\"{}\",\"reward\":\"{}\",\"completed\":{}}}",
+                    i,
+                    creator,
+                    assignee,
+                    String::from_utf8_lossy(description).replace("\"", "\\\""),  // Updated for Vec<u8>
+                    reward,
+                    completed
+                ));
+            }
+        }
+        all_tasks.push_str("]");
+        all_tasks
+    }
+}    
